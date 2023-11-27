7fa5ec915e7869a6fd2c4b606f64c735d619404d
mza-wtag <mahfuz.zaman@welldev.io>
2023-11-27 18:23:59 +0600

    create functionality added

--- script/app.js
+++ script/app.js
@@ -1,6 +1,16 @@
 let addToDoButton = document.getElementById("addToDo");
 let toDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
+let createTask = document.querySelector(".button_create");
+let taskCard = document.querySelector(".task__card");
+
+createTask.addEventListener("click", function () {
+    if (taskCard.style.display === "none") {
+        taskCard.style.display = "block";
+    } else {
+        taskCard.style.display = "none";
+    }
+});
 
 addToDoButton.addEventListener("click", function () {
     let paragraph = document.createElement("p");
