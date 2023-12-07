de7f82058528cbbabdbb39719732ed49f612eae4
mza-wtag <mahfuz.zaman@welldev.io>
2023-12-07 09:14:51 +0600

    file separate

---
+++ script/elements.js
@@ -0,0 +1,7 @@
+export const addToDoButton = document.querySelector(".task__button-add");
+export const toDoContainer = document.querySelector(".todo__container");
+export const todoInput = document.querySelector(".task__input");
+export const createTask = document.querySelector(".task-board__button--create");
+export const taskCardNew = document.querySelector(".task__card-new");
+export const taskContainer = document.querySelector(".task__container");
+export const taskCard = document.querySelector(".task__card");
